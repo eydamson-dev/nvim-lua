a18302de97d3d46f634c59a54148cbf504ee9988
Adamson Vispo <adamson.vispo@surefiresystems.com>
2021-12-22 16:13:34 +0800

    initial commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorschemes"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comments"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.alpha"
+require "user.whichkey"
+
+
+
+
